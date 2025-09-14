840851a6a9d758bfd310a0b9d17b385a1bcbd7d5
1freelipe <feliperodrigues_spfc@hotmail.com>
2025-09-14 17:38:42 -0300

    middleware para checar o JWT

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -15,6 +15,8 @@ class UsersController {
 
   async index(req, res) {
     try {
+      console.log('USER ID:', req.userId);
+      console.log('USER Email:', req.userEmail);
       const users = await User.findAll();
       return res.json(users);
     } catch (e) {
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import usersController from '../controllers/usersController';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const router = new Router();
 
 router.post('/', usersController.store);
-router.get('/', usersController.index);
+router.get('/', loginRequired, usersController.index);
 router.get('/:id', usersController.show);
 router.put('/:id', usersController.update);
 router.delete('/:id', usersController.delete);
